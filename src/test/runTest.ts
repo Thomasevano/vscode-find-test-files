1e7e1adc264a8a2848dbfe61b62cf1d0e4a20c45
Thomasevano <thomas-evano@hotmail.fr>
2023-07-06 11:03:26 +0200

    feat: add tests to extension

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -11,9 +11,15 @@ async function main() {
 		// The path to test runner
 		// Passed to --extensionTestsPath
 		const extensionTestsPath = path.resolve(__dirname, './suite/index');
+		const testWorkspace = path.resolve(__dirname, './exampleProject');
 
 		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+		await runTests({
+			version: 'insiders',
+			extensionDevelopmentPath,
+			extensionTestsPath,
+			launchArgs: [testWorkspace], 
+		});
 	} catch (err) {
 		console.error('Failed to run tests', err);
 		process.exit(1);
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,28 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import { getProjectRootFolder } from '../../extension';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  vscode.window.showInformationMessage('Start all tests.');
+  
+  test('getProjectRootFolder', async () => {
+    console.log('directoryName', __dirname);
+    
+    // given
+    const filePath = 'exampleProject/api/lib/infrastructure/user.js';
+    
+    // when
+    const projectRootPath = getProjectRootFolder(filePath);
+    
+    // then
+    assert.strictEqual(projectRootPath, 'api');
+  });
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  // test('find the corresponding test file', () => {
+  //   // given
+  //   const currentFile = '';
+  //   assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+  //   assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  // });
 });
